4aaf2de7e809386f39a7994b1b72b5fb14cc5280
leeseongwon95 <67531188+leeseongwon95@users.noreply.github.com>
2020-09-18 17:37:05 +0900

    toolkit

---
+++ src/store.js
@@ -0,0 +1,28 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE");
+
+// const reducer = createReducer([], {
+//     [addToDo]: (state, action) => {
+//         state.push({ text: action.payload, id: Date.now() });
+//     },
+//     [deleteToDo]: (state, action) =>
+//         state.filter(toDo => toDo.id !== action.payload),
+// });
+
+const toDos = createSlice({
+    name: "toDosReducer",
+    initialState: [],
+    reducers: {
+        add: (state, action) => {
+            state.push({ text: action.payload, id: Date.now() });
+        },
+        remove: (state, action) =>
+            state.filter(toDo => toDo.id !== action.payload),
+    },
+});
+
+export const { add, remove } = toDos.actions;
+
+export default configureStore({ reducer: toDos.reducer });
